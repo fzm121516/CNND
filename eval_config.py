caf61f2f5553aa987a86177ab07822c285cf074a
fan <3223643263@qq.com>
2024-11-11 17:48:33 +0800

    1111

--- eval_config.py
+++ eval_config.py
@@ -2,18 +2,25 @@ from util import mkdir
 
 
 # directory to store the results
-results_dir = './results/'
+results_dir = './results1/'
 mkdir(results_dir)
 
+# # root to the testsets
+# dataroot = '/home/fanzheming/zm/NPR-DeepfakeDetection/dataset/ForenSynths'
+
+# # list of synthesis algorithms
+# vals = ['progan', 'stylegan', 'biggan', 'cyclegan', 'stargan', 'gaugan',
+#         'crn', 'imle', 'seeingdark', 'san', 'deepfake', 'stylegan2', 'whichfaceisreal']
+# # indicates if corresponding testset has multiple classes
+# multiclass = [1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0]
 # root to the testsets
-dataroot = '/home/fanzheming/zm/NPR-DeepfakeDetection/dataset/ForenSynths'
+dataroot = '/home/fanzheming/zm/NPR-DeepfakeDetection/dataset/UniversalFakeDetect'
 
 # list of synthesis algorithms
-vals = ['progan', 'stylegan', 'biggan', 'cyclegan', 'stargan', 'gaugan',
-        'crn', 'imle', 'seeingdark', 'san', 'deepfake', 'stylegan2', 'whichfaceisreal']
+vals = [ 'glide_100_10', 'ldm_200_cfg', 'glide_50_27' ,'ldm_100', 'glide_100_27', 'dalle', 'ldm_200', 'guided']
 
 # indicates if corresponding testset has multiple classes
-multiclass = [1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0]
+multiclass = [0, 0, 0, 0, 0, 0, 0, 0]
 
 # model
 model_path = 'weights/blur_jpg_prob0.1.pth'
